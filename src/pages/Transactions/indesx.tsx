414e4628db141dfa5cf9ae564f8237859d6ae12a
paulobarbosah@hotmail.com <paulobarbosah@hotmail.com>
2023-11-06 16:44:57 -0300

    Tabela de transações

--- src/pages/Transactions/indesx.tsx
+++ src/pages/Transactions/indesx.tsx
@@ -1,11 +1,40 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionTable, TransactionsContainer } from "./styles";
 
 export function Transaction(){
     return(
         <div>
             <Header/>
             <Summary/>
+
+            <TransactionsContainer>
+                <TransactionTable>
+                    <tbody>
+                        <tr>
+                            <td>Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+
+                        <tr>
+                            <td>Alimentação</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    -R$ 1.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Supermercado</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionTable>
+            </TransactionsContainer>
         </div>
     )
 }
