29176297332c46bc3c29c20ed5d82ba443a6935d
paulobarbosah@hotmail.com <paulobarbosah@hotmail.com>
2023-11-06 17:44:14 -0300

    add modal Radix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,27 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logo from '../../assets/dt-money.svg'
 
+import * as Dialog from '@radix-ui/react-dialog';
+
 export function Header(){
     return(
         <HeaderContainer>
             <HeaderContent>
                 <img src={logo} alt="" />
-                <NewTransactionButton>Nova Transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>
+                            Nova Transação
+                        </NewTransactionButton>
+                    </Dialog.Trigger>
+                    <Dialog.DialogPortal>
+                        <Dialog.Overlay/>
+                        <Dialog.DialogContent>
+                            <Dialog.DialogTitle> Nova Transação</Dialog.DialogTitle>
+                            <Dialog.Close/>
+                        </Dialog.DialogContent>
+                    </Dialog.DialogPortal>
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
