ae05637cc7daf8805313416a86c0f86db0fb9290
paulobarbosah@hotmail.com <paulobarbosah@hotmail.com>
2024-01-10 13:11:18 -0300

    memo
    Usando o memo na performance da interface, onde ele pode ser usado para evitar a renderização de componentes.  Obs.: o uso processo que o memo usa pode deixar a aplicação lenta. Usar em casos específicos.

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -5,6 +5,7 @@ import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
+import { memo } from 'react'
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -12,7 +13,7 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export function SearchFrom() {
+function SearchFromComponent() {
   const fetchTransaction = useContextSelector(
     TransactionsContext,
     (context) => {
@@ -45,3 +46,5 @@ export function SearchFrom() {
     </SearchFormContainer>
   )
 }
+
+export const SearchFrom = memo(SearchFromComponent)
